9700ff61de59deb79aab27c0171aac36ab349e49
Drew Carlson <awcarlson@protonmail.com>
2021-11-20 23:11:44 -0800

    Use kotlin multiplatform plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version KOTLIN_VERSION
+    kotlin("multiplatform") version KOTLIN_VERSION
     kotlin("plugin.serialization") version KOTLIN_VERSION
     id("org.jetbrains.dokka") version DOKKA_VERSION
 }
@@ -10,15 +10,27 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    implementation(kotlin("stdlib"))
-    implementation("io.ktor:ktor-server-core:$KTOR_VERSION")
-    implementation("io.ktor:ktor-server-sessions:$KTOR_VERSION")
-    implementation("io.ktor:ktor-auth:$KTOR_VERSION")
-
-    testImplementation(kotlin("test"))
-    testImplementation(kotlin("test-junit"))
-    testImplementation("io.ktor:ktor-server-tests:$KTOR_VERSION")
-    testImplementation("io.ktor:ktor-serialization:$KTOR_VERSION")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$SERIALIZATION_VERSION")
+kotlin {
+    jvm()
+    sourceSets {
+        val jvmMain by getting {
+            kotlin.srcDir("src/main/kotlin")
+            dependencies {
+                implementation(kotlin("stdlib"))
+                implementation("io.ktor:ktor-server-core:$KTOR_VERSION")
+                implementation("io.ktor:ktor-server-sessions:$KTOR_VERSION")
+                implementation("io.ktor:ktor-auth:$KTOR_VERSION")
+            }
+        }
+        val jvmTest by getting {
+            kotlin.srcDir("src/test/kotlin")
+            dependencies {
+                implementation(kotlin("test"))
+                implementation(kotlin("test-junit"))
+                implementation("io.ktor:ktor-server-tests:$KTOR_VERSION")
+                implementation("io.ktor:ktor-serialization:$KTOR_VERSION")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$SERIALIZATION_VERSION")
+            }
+        }
+    }
 }
